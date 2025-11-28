681e13f0ca210da25eaa163a0b3f1ac694a13ef0
Veli ÇIRAGÖZ <veli_ciragoz@hotmail.com>
2025-11-28 23:47:50 +0300

    skor ve sayac eklendi.

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+import turtle
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+score_puan=0
+times=15
+FONT_SIZE = 18
+FONT = ('Arial', FONT_SIZE, 'bold')
+
+drawing_board=turtle.Screen()
+drawing_board.bgcolor("green")
+drawing_board.title("Turtle Drawing")
+drawing_board.title("Turtle Drawing Screen")
+
+turtle_instance=turtle.Turtle()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+turtle_instance.penup()
+turtle_instance.goto(0,350)
+turtle_instance.write("Score "+str(score_puan), align='center',font=FONT)
+turtle_instance.penup()
+turtle.setposition(0, 0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+pen_sayac=turtle.Turtle()
+
+def sayac():
+    global times
+    global score_puan
+    while times>0:
+        time.sleep(1)
+        pen_sayac.clear()
+        pen_sayac.penup()
+        pen_sayac.goto(0, 320)
+        pen_sayac.write("Time : " + str(times), align='center', font=FONT)
+        times-=1
+        if times == 0:
+            times = 15
+            score_puan=0
+
+
+
+
+def temizle():
+    print("temizle")
+    pen_sayac.clear()
+    global times
+    global score_puan
+    score_puan=0
+    times=15
+
+sayac()
+
+
+drawing_board.listen()
+
+drawing_board.onkey(fun=temizle,key="space")
+
+
+drawing_board.mainloop()
